1469a80706049645be640da450a5e365bc5df7aa
wiaamOSC <walsalihi@ormiston.school.nz>
2021-02-15 14:50:54 +1300

    Add files via upload

---
+++ Course_Selection_Interface.py
@@ -0,0 +1,6 @@
+#writer name: Al Salihi
+#Version Number : 1
+
+teacher_name=input("Enter the name of the teacher? ")
+print("The teacher name is ",teacher_name)
+Student_name=input ("What is the name of the student")
